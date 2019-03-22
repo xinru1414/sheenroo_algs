67ed256d1c72add26b1ed75eb1fc19db52083c5d
Xinru Yan 🇨🇳  👑 <xinru1414@gmail.com>
2019-03-21 21:12:24 -0400

    First Commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(name='sheenroo_algs',
+      version='1.0',
+      description='Ein Python-Modul für verschiedene Algorithmen',
+      url='https://github.com/xinru1414/sheenroo_algs',
+      author='SheenRoo die chinesische Prinzessin',
+      author_email='xinru1414@gmail.com',
+      license='MIT',
+      packages=setuptools.find_packages(),
+      zip_safe=False)
